a3834579a9a27a2afbaedd51f5fbb015940324b6
Dmitry Kushniruk <dmytro@kushniruk.dev>
2024-10-02 12:10:37 +0300

    chore(frontend): add @tanstack/react-query for data fetching

--- packages/frontend/src/app/index.tsx
+++ packages/frontend/src/app/index.tsx
@@ -1,12 +1,19 @@
 import { FC } from "react";
 import { RouterProvider } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import { router } from "./routes";
 
 import "./index.css";
 
+const queryClient = new QueryClient();
+
 const App: FC = () => {
-  return <RouterProvider router={router} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  );
 };
 
 export default App;

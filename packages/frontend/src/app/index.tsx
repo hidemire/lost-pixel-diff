30b894301a69e19f82f7efd7a0ffaa156b8576e2
Dmitry Kushniruk <dmytro@kushniruk.dev>
2024-10-02 12:12:31 +0300

    chore(frontend): add sonner toaster

--- packages/frontend/src/app/index.tsx
+++ packages/frontend/src/app/index.tsx
@@ -2,6 +2,8 @@ import { FC } from "react";
 import { RouterProvider } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+import { Toaster } from "~/components/ui/sonner";
+
 import { router } from "./routes";
 
 import "./index.css";
@@ -12,6 +14,7 @@ const App: FC = () => {
   return (
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
+      <Toaster />
     </QueryClientProvider>
   );
 };

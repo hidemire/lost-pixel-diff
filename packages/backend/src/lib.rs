d897ffc8667ce2005130ebd4ce40672e95d9c753
Dmitry Kushniruk <dmytro@kushniruk.dev>
2024-10-02 11:08:27 +0300

    feat(backend): add tracing_subscriber

--- packages/backend/src/lib.rs
+++ packages/backend/src/lib.rs
@@ -1,2 +1,3 @@
 pub mod settings;
 pub mod startup;
+pub mod telemetry;
--- packages/backend/src/main.rs
+++ packages/backend/src/main.rs
@@ -1,9 +1,22 @@
-use backend::{settings::Settings, startup::Application};
+use backend::{
+    settings::Settings,
+    startup::Application,
+    telemetry::{get_subscriber, init_subscriber},
+};
+use tracing::level_filters::LevelFilter;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let subscriber = get_subscriber(LevelFilter::INFO, std::io::stdout);
+    init_subscriber(subscriber);
+
     let settings = Settings::new()?;
+
     let application = Application::build(settings)?;
+    tracing::info!("Listening on {}:{}", application.ip(), application.port());
+
     application.run().await?;
+    tracing::info!("Exiting...");
+
     Ok(())
 }
